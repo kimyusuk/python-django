069d8affbf07d320cd70f7b6e540943a139f646f
kim yu suk <airheart0820@gmail.com>
2023-02-02 15:06:45 +0900

    crawling

--- pybo/tests.py
+++ pybo/tests.py
@@ -1,3 +1,83 @@
 from django.test import TestCase
+import unittest
+import datetime
+import logging
+import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+class Crawling(unittest.TestCase):
+    def setup(self):
+        print("setUp")
+    def test_naver_stock(self):
+        codes = {"삼성전자":"005930","현대차":"005380"}
+        for i in codes.keys():
+            url = "https://finance.naver.com/item/main.naver?code="
+            urlc = url + str(codes[i])
+            response = requests.get(urlc)
+            if 200 == response.status_code:
+                html = response.text
+                bs4 = BeautifulSoup(html, "html.parser")
+                price = bs4.select("div.today p.no_today span.blind")
+                print("종목명: {},코드: {},주가: {}".format(i,codes[i],price[0].getText()))
+            else:
+                print("접속 오류")
 
-# Create your tests here.
+
+    @unittest.skip("테스트 연습")
+    def tearDown(self):
+        print("tearDown")
+    @unittest.skip("테스트 연습")
+    def call_slemdunk(self,url):
+        # url = "https://movie.naver.com/movie/point/af/list.naver?st=mcode&sword=223800&target=after&page=1"
+        response = requests.get(url)
+        if 200 == response.status_code:
+            html = response.text
+            bs4 = BeautifulSoup(html, "html.parser")
+            score = bs4.select("div.list_netizen_score em")
+            review = bs4.select("table tbody tr td.title")
+            for i in range(0,len(score)):
+                review_text = review[i].getText().split('\n')
+                if len(review_text)>2:
+                    tmp_text = review_text[5]
+                else:
+                    tmp_text = review_text[0]
+                print("평점 감상평:{}, {}".format(score[i].getText(),tmp_text))
+    @unittest.skip("테스트 연습")
+    def test_slemdunk(self):
+        url = "https://movie.naver.com/movie/point/af/list.naver?st=mcode&sword=223800&target=after&page=1"
+        for i in range(1, 4, 1):
+            self.call_slemdunk(url + str(i))
+    @unittest.skip("테스트 연습")
+    def test_cgv(self):
+        #cgv = http://www.cgv.co.kr/movies/?lt=1&ft=0
+        url = "http://www.cgv.co.kr/movies/?lt=1&ft=0"
+        response = requests.get(url)
+        print(response.status_code)
+        if 200 == response.status_code:
+            html = response.text
+            bs4 = BeautifulSoup(html, "html.parser")
+            title = bs4.select("div.box-contents strong.title")
+            reserve = bs4.select("div.score strong.percent span")
+            poster = bs4.select("span.thumb-image img")
+            for i in range(0,7,1):
+                posterImg = poster[i]
+                imgUrlPath = posterImg.get('src')
+                # print(imgUrlPath)
+                print("영화:{}, {}, {}".format(title[i].getText(),reserve[i].getText(),imgUrlPath))
+        else:
+            print("접속오류 response.status_code:{}".format(response.status_code))
+    @unittest.skip("테스트 연습")
+    def test_weather(self):
+        #날씨 = https://weather.naver.com/today/02113132?cpName=KMA
+        now = datetime.datetime.now()
+        #yyyymmdd hh:mm
+        newDate = now.strftime("%Y-%m-%d %H:%M:%S")
+        print("="*35)
+        print(newDate)
+        print("=" * 35)
+        naverWeatherUrl ="https://weather.naver.com/today/09545101"
+        html = urlopen(str(naverWeatherUrl))
+        print(html)
+        bsObj = BeautifulSoup(html, "html.parser")
+        temps = bsObj.find('strong', 'current')
+        print("서울 마포구 서교동 날씨:{}".format(temps.getText()))
--- pybo/views.py
+++ pybo/views.py
@@ -4,6 +4,8 @@ from .models import  Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 import logging
+from bs4 import BeautifulSoup
+import requests
 
 
 def boot_menu(request):
@@ -71,3 +73,30 @@ def boot_list(request):
 
 def boot_reg(request):
     return render(request, "pybo/reg.html")
+
+def crawling_cgv(request):
+    url = "http://www.cgv.co.kr/movies/?lt=1&ft=0"
+    response = requests.get(url)
+    print(response.status_code)
+    context = {}
+    if 200 == response.status_code:
+        html = response.text
+        bs4 = BeautifulSoup(html, "html.parser")
+        title = bs4.select("div.box-contents strong.title")
+        reserve = bs4.select("div.score strong.percent span")
+        poster = bs4.select("span.thumb-image img")
+        title_list = []
+        reserve_list = []
+        poster_list = []
+        for i in range(0,7,1):
+            posterImg = poster[i]
+            imgUrlPath = posterImg.get('src')
+            title_list.append(title[i].getText()) #제목
+            reserve_list.append(reserve[i].getText()) #예매율
+            poster_list.append(imgUrlPath) #포스터 소스
+            print("영화:{}, {}, {}".format(title[i].getText(),reserve[i].getText(),imgUrlPath))
+        context = {"title": title_list, 'reserve': reserve_list, 'poster' : poster_list}
+    else:
+        print("접속오류 response.status_code:{}".format(response.status_code))
+
+    return render(request, "pybo/crawling_cgv.html", context)
