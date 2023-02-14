27cbbb42256a7c068fddb4dc74c6402b078046db
kimyusuk <airheart0820@gmail.com>
2023-02-14 11:04:04 +0900

    주소 세팅 나누기

---
+++ config/settings/local.py
@@ -0,0 +1,3 @@
+from .base import * # base에 있는 모든 내용을 사용한다. allowed_host만 따로 사용하겠다 라는뜻
+
+ALLOWED_HOSTS = []
---
+++ config/settings/prod.py
@@ -0,0 +1,4 @@
+from .base import * # base에 있는 모든 내용을 사용한다. allowed_host만 따로 사용하겠다 라는뜻
+
+
+ALLOWED_HOSTS = ["192.168.219.103"] # 나중에 될 서버 ip
